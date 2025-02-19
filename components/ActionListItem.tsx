49225080864d3a775ccd92c20df0f3c0297639d3
Stephen Handley <stephen@hello10.com>
2025-02-19 02:16:30 -0800

    fix spacing for ActionListItem

--- components/ActionListItem.tsx
+++ components/ActionListItem.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 const styles = {
-  item: "flex items-start justify-between bg-transparent text-[var(--theme-text)] cursor-pointer outline-0 border-0 no-underline group !no-underline",
-  icon: "!flex items-center justify-center bg-[var(--theme-button-foreground)] flex-shrink-0 w-[3ch] h-[calc(var(--font-size)*var(--theme-line-height-base))] select-none group-hover:bg-[var(--theme-focused-foreground)] group-focus:bg-[var(--theme-focused-foreground)]",
-  text: "inline-flex items-center justify-start self-stretch bg-[var(--theme-button-background)] min-w-[10%] w-full px-[1ch] select-none"
+  item: "flex items-center justify-between bg-transparent text-[var(--theme-text)] cursor-pointer outline-0 border-0 no-underline group !no-underline h-[calc(var(--theme-line-height-base)*1rem)]",
+  icon: "flex items-center justify-center bg-[var(--theme-button-foreground)] flex-shrink-0 w-[3ch] h-full select-none group-hover:bg-[var(--theme-focused-foreground)] group-focus:bg-[var(--theme-focused-foreground)]",
+  text: "inline-flex items-center justify-start bg-[var(--theme-button-background)] min-w-[10%] w-full px-[1ch] select-none h-full"
 };
 
 export interface ActionListItemProps {
